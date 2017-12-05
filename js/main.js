a6212e8a93b970cbc3ca0607cd503348d0acf67e
GeekishGirl <tusek.nicole@gmail.com>
2017-12-05 13:22:10 -0800

    Updated CSS Stuffs.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
+alert("test");
+
 var game = new Phaser.game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 /**
  * @desc Preload all the resources.
  */
 function preload() {
+    game.load.image('sky', 'assets/sky.png');
+    game.load.image('ground', 'assets/platform.png');
+    game.load.image('star', 'assets/star.png');
 
+    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 
 /**
  * @desc Create all the stuff needed for the game.
  */
 function create() {
-
+    game.add.sprite(0, 0, 'star');
 }
 
 /**
