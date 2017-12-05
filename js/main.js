fce1fbd08af116e2da5029c4298b0d3676d335ba
GeekishGirl <tusek.nicole@gmail.com>
2017-12-05 13:11:36 -0800

    Added boilerplate resources for game.

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var game = new Phaser.game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+/**
+ * @desc Preload all the resources.
+ */
+function preload() {
+
+}
+
+/**
+ * @desc Create all the stuff needed for the game.
+ */
+function create() {
+
+}
+
+/**
+ * @desc Update the game and do the game process stuff.
+ */
+function update() {
+
+}
